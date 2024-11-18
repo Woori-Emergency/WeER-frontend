6aa68bee8237439b8794558aca0612d0ca8b78a9
ayleeee <janhyang0309@gmail.com>
2024-11-18 16:01:10 +0900

    refactor: 예약페이지 및 환자 상태 내역 페이지 개선

--- src/components/patientStatus/PatientInfoCardList.js
+++ src/components/patientStatus/PatientInfoCardList.js
@@ -4,8 +4,9 @@ import { formatDate } from '../../utils/dateUtils';
 
 const Card = styled.div`
   background: white;
+  border-radius: 0.5rem;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
   overflow: hidden;
-  border-bottom: 1px solid transparent;
 `;
 
 const CardHeader = styled.div`
